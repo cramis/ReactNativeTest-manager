95be447d9522a0de5d6e05b4122838c10ff742b8
박진억 <cramis@gmail.com>
2018-07-03 18:09:55 +0900

    picker 추가

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
+import { Picker, Text } from 'react-native';
 
 import { Card, CardSection, Button, Input } from './common';
 
@@ -24,6 +25,24 @@ class EmployeeCreate extends Component {
                     value={this.props.phone}
                     />
                 </CardSection>
+                <CardSection style={{ flexDirection: 'column' }}>
+                    <Text style={styles.pickerTextStyle}>Shift</Text>
+                    <CardSection>
+                        <Picker
+                            style={ styles.pickerStyle }
+                            selectedValue={this.props.shift}
+                            onValueChange={value => this.props.employeeUpdate({ prop: 'shift', value })}
+                        >
+                            <Picker.Item label='Monday' value='Monday' />
+                            <Picker.Item label='Tuesday' value='Tuesday' />
+                            <Picker.Item label='Wednesday' value='Wednesday' />
+                            <Picker.Item label='Thursday' value='Thursday' />
+                            <Picker.Item label='Friday' value='Friday' />
+                            <Picker.Item label='Saturday' value='Saturday' />
+                            <Picker.Item label='Sunday' value='Sunday' />                        
+                        </Picker>
+                    </CardSection>                    
+                </CardSection>  
                 <CardSection>
                     <Button>
                         Save
@@ -33,6 +52,22 @@ class EmployeeCreate extends Component {
         );
     }
 }
+
+const styles = {
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+      },
+    pickerStyle: {
+        backgroundColor: '#9F9F9F',
+        height: 40,
+        alignSelf: 'stretch', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        flex: 1
+    }
+        
+  };
 
 const mapStateToProps = ({ employeeForm }) => {
     const { name, phone, shift } = employeeForm;
